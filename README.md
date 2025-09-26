1f99a6cfc4b32b886bfc2c2b8f7dc0d9c9d9732e
Graeme Winter <graeme.winter@gmail.com>
2025-09-26 10:46:46 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Beam Status Monitor
+
+Something to monitor the APS beam status and write big changes to a Slack workspace.
+
+## Dependencies
+
+```console
+pip3 install slack-api slack-sdk pyepics
+```
+
+EPICS repeater:
+
+```console
+export EPICS_CA_ADDR_LIST=164.54.212.49
+```
+
+API key:
+
+```console
+SLACK_BOT_TOKEN=xoxb-fixme-token python3 test.py
+```
---
+++ monitor.py
@@ -0,0 +1,30 @@
+import datetime
+import os
+import time
+
+import epics
+
+from slack_sdk import WebClient
+
+client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+
+oldvalue = 0.0
+
+def callback(pvname, value, timestamp, **kwargs):
+    global oldvalue
+    ts = datetime.datetime.fromtimestamp(timestamp)
+    if abs(value - oldvalue) < 5:
+        return
+    oldvalue = value
+    try:
+        response = client.chat_postMessage(
+            channel="C09H79FD0R4", text=f"Beam current at {ts.strftime("%Y-%m-%d %H:%M:%S")}: {value:.1f}mA"
+        )
+    except:
+        pass
+
+
+epics.camonitor("S-DCCT:CurrentM", callback=callback, monitor_delta=10)
+
+while 1:
+    time.sleep(100)
