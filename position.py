6e8724574e997ad35d9dbf9072f8c9001b9d4a36
Graeme Winter <graeme.winter@gmail.com>
2025-09-29 11:45:47 -0500

    Tie in Slack; 1µm positional

--- position.py
+++ position.py
@@ -1,10 +1,17 @@
+import datetime
+import os
 import time
-
-import epics
 
 import numpy
 
+import epics
+
+from slack_sdk import WebClient
+
+client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+
 NN = 100
+THRESH = 1.0
 
 bpm_x = numpy.zeros(shape=(NN,), dtype=numpy.float32)
 bpm_y = numpy.zeros(shape=(NN,), dtype=numpy.float32)
@@ -18,6 +25,17 @@ bpm_y_mean = 0.0
 bpm_y_sd = 0.0
 
 
+def transmit(message, timestamp):
+    ts = datetime.datetime.fromtimestamp(timestamp)
+    try:
+        response = client.chat_postMessage(
+            channel="C09H79FD0R4",
+            text=message,
+        )
+    except:
+        pass
+
+
 def callback(pvname, value, timestamp, **kwargs):
     global bpm_x, bpm_y, n_bpm_x, n_bpm_y, NN, bpm_x_mean, bpm_x_sd, bpm_y_mean, bpm_y_sd
 
@@ -25,45 +43,49 @@ def callback(pvname, value, timestamp, **kwargs):
         bpm_x[n_bpm_x % NN] = value
         n_bpm_x += 1
 
-        if n_bpm_x < 100:
+        if n_bpm_x < NN:
             return
 
         mean, sd = numpy.mean(bpm_x), numpy.sqrt(numpy.var(bpm_x))
 
-        if abs(mean - bpm_x_mean) > 0.5 and abs(sd - bpm_x_sd) > 0.5:
-            print(f"X position unstable in µ, σ: {mean:.1f} {sd:.1f}")
+        if abs(mean - bpm_x_mean) > THRESH and abs(sd - bpm_x_sd) > THRESH:
+            transmit(f"X position unstable in µ, σ: {mean:.1f} {sd:.1f}", timestamp)
             bpm_x_mean = mean
             bpm_x_sd = sd
-        elif abs(mean - bpm_x_mean) > 0.5:
-            print(f"X position unstable in µ: {mean:.1f}")
+        elif abs(mean - bpm_x_mean) > THRESH:
+            transmit(f"X position unstable in µ: {mean:.1f}", timestamp)
             bpm_x_mean = mean
-        elif abs(sd - bpm_x_sd) > 0.5:
-            print(f"X position unstable in σ: {sd:.1f}")
+        elif abs(sd - bpm_x_sd) > THRESH:
+            transmit(f"X position unstable in σ: {sd:.1f}", timestamp)
             bpm_x_sd = sd
 
     elif pvname == "S24IDFE-XBPM:P1us:y":
         bpm_y[n_bpm_y % NN] = value
         n_bpm_y += 1
 
-        if n_bpm_y < 100:
+        if n_bpm_y < NN:
             return
 
         mean, sd = numpy.mean(bpm_y), numpy.sqrt(numpy.var(bpm_y))
 
-        if abs(mean - bpm_y_mean) > 0.5 and abs(sd - bpm_y_sd) > 0.5:
-            print(f"Y position unstable in µ, σ: {mean:.1f} {sd:.1f}")
+        if abs(mean - bpm_y_mean) > THRESH and abs(sd - bpm_y_sd) > THRESH:
+            transmit(f"Y position unstable in µ, σ: {mean:.1f} {sd:.1f}", timestamp)
             bpm_y_mean = mean
             bpm_y_sd = sd
-        elif abs(mean - bpm_y_mean) > 0.5:
-            print(f"Y position unstable in µ: {mean:.1f}")
+        elif abs(mean - bpm_y_mean) > THRESH:
+            transmit(f"Y position unstable in µ: {mean:.1f}", timestamp)
             bpm_y_mean = mean
-        elif abs(sd - bpm_y_sd) > 0.5:
-            print(f"Y position unstable in σ: {sd:.1f}")
+        elif abs(sd - bpm_y_sd) > THRESH:
+            transmit(f"Y position unstable in σ: {sd:.1f}", timestamp)
             bpm_y_sd = sd
 
 
 epics.camonitor("S24IDFE-XBPM:P1us:x", callback=callback)
 epics.camonitor("S24IDFE-XBPM:P1us:y", callback=callback)
+
+transmit(
+    "Monitoring S24IDFE-XBPM:P1us:x and S24IDFE-XBPM:P1us:y", timestamp=time.time()
+)
 
 while True:
     time.sleep(1)
